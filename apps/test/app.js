6db5e234e548af4df647b0b52836511dc342e2e7
Xose Rodriguez Gonzalez <>
2021-01-31 13:13:53 +0100

    RunningTools

--- apps/test/app.js
+++ apps/test/app.js
@@ -1,3 +1,5 @@
+Bangle.setLCDMode("doublebuffered");
+
 E.showMessage("My\nSimple\nApp","My App")
 
 setWatch(function(e) {
@@ -10,3 +12,17 @@ setWatch(function(e) {
      Bangle.beep(10);
     E.showMessage("Right","My App");
 }, BTN5, { edge:"falling",repeat:true,debounce:50});
+
+
+setWatch(function(e) {
+     Bangle.beep(10);
+    E.showMessage("BTN2","My App");
+}, BTN2, {});
+
+
+
+if ( BTN1.read() ) {
+     Bangle.beep(10);
+     E.showMessage("BTN1","My App");
+     console.log("BTN1");
+}
