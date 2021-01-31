aeacb8f54d8aca3ecb75f6b6c5c96b1cda1fd5b4
Xose Rodriguez Gonzalez <>
2021-01-31 02:21:02 +0100

    no message

--- apps/test/app.js
+++ apps/test/app.js
@@ -1,1 +1,10 @@
 E.showMessage("My\nSimple\nApp","My App")
+
+setWatch(function(e) {
+    E.showMessage("Left","My App")
+}, BTN4, { edge:"falling",repeat:true,debounce:50});
+
+
+setWatch(function(e) {
+    E.showMessage("Right","My App")
+}, BTN5, { edge:"falling",repeat:true,debounce:50});
