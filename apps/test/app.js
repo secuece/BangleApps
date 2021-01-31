010d588efca49928f8b7b866a5dbfb97428b4011
Xose Rodriguez Gonzalez <>
2021-01-31 13:02:57 +0100

    Updated packageRunningTools

--- apps/test/app.js
+++ apps/test/app.js
@@ -1,12 +1,14 @@
 E.showMessage("My\nSimple\nApp","My App")
 
 setWatch(function(e) {
-     Bangle.beep();
+     Bangle.buzz(1000);
+     Bangle.beep(200,207.65*8);
     E.showMessage("Left","My App");
 }, BTN4, { edge:"falling",repeat:true,debounce:50});
 
 
 setWatch(function(e) {
-     Bangle.beep();
+     Bangle.buzz(1000);
+     Bangle.beep(200,207.65*8);
     E.showMessage("Right","My App");
 }, BTN5, { edge:"falling",repeat:true,debounce:50});
