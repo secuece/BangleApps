f102278fa4aea045a79c1b6246ec2acc3d9aacde
secuece <78283768+secuece@users.noreply.github.com>
2021-02-02 23:37:37 +0100

    Update app.js

--- apps/test/app.js
+++ apps/test/app.js
@@ -1,21 +1,23 @@
 Bangle.setGPSPower(1);
 
-const gpsState = false;
+function onGPS(gpsData) {
+  g.clear();
+  g.setColor(50712);
+  g.setFont("6x8", 2);
+  g.drawString("DIST (KM)", 5, 5);
 
-g.clear();
-g.setColor(50712);
-g.setFont("6x8", 2);
-g.drawString("DIST (KM)", 5, 5);
-
-g.setFont("6x8", 1);
-g.setColor(gpsState ? 2016 : 63488);
-g.drawString("GPS", 220, 5);
-
-
-
-
-Bangle.on("GPS", function (gpsData) {
-  g.drawString(gpsData.lat.toFixed(0), 220, 10);
-  g.drawString(gpsData.lon.toFixed(0), 220, 15);
+  g.setFont("6x8", 1);
+  g.setColor(gpsData ? 2016 : 63488);
+  g.drawString("GPS", 220, 5);
+  
+  if ( gpsData ) {
+    g.drawString(gpsData.lat.toFixed(0), 220, 10);
+    g.drawString(gpsData.lon.toFixed(0), 220, 15);
+  }
+  
   g.flip();
-});
+  
+}
+
+
+Bangle.on("GPS", onGPS);
