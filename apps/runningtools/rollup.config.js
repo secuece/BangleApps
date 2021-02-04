aedf26945202a9a158d2c1f608dc5de0aaf35a30
Xose Rodriguez Gonzalez <secuece@gmail.com>
2021-02-04 02:46:49 +0100

    Updated runningtools

--- apps/runningtools/rollup.config.js
+++ apps/runningtools/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: './src/main.ts',
+  input: './src/app.ts',
   output: {
     dir: '.',
     format: 'iife',
